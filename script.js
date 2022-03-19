15ac816e53f80fce07f600bdfb3e928b96c3f857
Rafael Herrera <rafaelherrera731@gmail.com>
2022-03-18 21:05:55 -0700

    simple HTML and CSS and some Javascript code

---
+++ script.js
@@ -0,0 +1,24 @@
+var startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startQuiz)
+
+function startQuiz (){
+console.log("started")
+}
+
+function startTimer(){
+
+}
+
+function correctAnswers(){
+
+}
+
+function incorrectAnswers(){
+
+}
+
+function finalScore(){
+
+}
+
